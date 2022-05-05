8c0b0849c474095f1dcf7fc5f47bdd91d3547179
aeromosaico <contato@aeromosaico.com.br>
2022-05-04 23:04:33 -0400

    many checks implementations

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const { argv } = require("process");
 const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+dayjs.extend(utc);
+const exifr = require("exifr");
 
 const getEvents = async (file) => {
   try {
@@ -9,17 +12,26 @@ const getEvents = async (file) => {
     for (let line of file.trim().split("\n")) {
       if (line.includes("%", 0)) continue;
       let s = line.trim().split(/\s+/);
+      // check if array placement is right
       if (
         +s[2] >= -90 &&
         +s[2] <= 90 &&
         +s[3] >= -180 &&
         +s[3] <= 180 &&
-        +s[4] > -1000 &&
-        +s[4] < 20000
+        +s[4] > 0 &&
+        +s[4] < 5000
       ) {
-        events.push([dayjs(s[0] + s[1]), s[2], s[3], s[4], s[7], s[8], s[9]]);
+        events.push([
+          dayjs(s[0].replaceAll("/", "-") + "T" + s[1] + "Z"),
+          s[2],
+          s[3],
+          s[4],
+          s[7],
+          s[8],
+          s[9],
+        ]);
       } else {
-        throw new SyntaxError("Invalid data");
+        throw new SyntaxError("Invalid pos data");
       }
     }
     return events;
@@ -29,57 +41,66 @@ const getEvents = async (file) => {
 };
 
 const getPics = async (picsPath) => {
-  const files = await fs.promises.readdir(picsPath);
-  const pics = [];
-  for (const file of files) {
-    const p = path.join(picsPath, file);
-    const stat = await fs.promises.stat(p);
-    pics.push([file, stat.mtime]);
+  try {
+    const files = await fs.promises.readdir(picsPath);
+    const pics = [];
+    for (const file of files) {
+      const p = path.join(picsPath, file);
+      const exifTime = await exifr.parse(p);
+      const time = exifTime["DateTimeOriginal"];
+      pics.push([file, time]);
+    }
+    return pics;
+  } catch (e) {
+    return console.log(e);
   }
-  return pics;
 };
 
-const arr2csv = (arrayHeader, events, pics) => {
+const arr2csv = (events, pics) => {
   let str = "name,lat,lon,height,sdn,sde,sdu\n";
-  let prevDiffTime;
+  // store temp previous difference time from event and pic and if is less or equal 1 seconds return error
+  let arrDiffTime = [];
   for (let i = 0; i < events.length; i++) {
     const eventTime = events[i][0];
     const picTime = pics[i][1];
-    const diffTime = eventTime.diff(picTime);
-    if (!prevDiffTime) prevDiffTime = diffTime;
-    //seconds
-    if (diffTime - prevDiffTime > 1001 * 1000)
-      return console.log(
-        `Image ${pics[i][0]} and respective event timestamp doesnt match`
+    const diffTime = Math.abs(eventTime.diff(picTime));
+    console.log(eventTime["$d"], picTime, diffTime);
+    arrDiffTime.push(diffTime);
+    const average = arrDiffTime.reduce((a, b) => a + b, 0) / arrDiffTime.length;
+    if (diffTime >= average - 1000 && diffTime <= average + 1000)
+      str +=
+        pics[i][0] +
+        "," +
+        events[i][1] +
+        "," +
+        events[i][2] +
+        "," +
+        events[i][3] +
+        "," +
+        events[i][4] +
+        "," +
+        events[i][5] +
+        "," +
+        events[i][6] +
+        "\n";
+    else
+      return console.error(
+        `${pics[i][0]} and event time row ${i} doesnt match, consider to shift`
       );
-    str +=
-      pics[i][0] +
-      "," +
-      events[i][1] +
-      "," +
-      events[i][2] +
-      "," +
-      events[i][3] +
-      "," +
-      events[i][4] +
-      "," +
-      events[i][5] +
-      "," +
-      events[i][6] +
-      "\n";
-    prevDiffTime = diffTime;
   }
-  fs.writeFileSync(argv[3] + "ppk.txt", str);
+  // check if the range of first and last is more than 2s (1 for less and 1 for more)
+  fs.writeFileSync("ppk.txt", str);
 };
 
 (async (posFile, picsPath) => {
   try {
-    const arrayHeader = ["name", "lat", "lon", "height", "sdn", "sde", "sdu"];
     const events = await getEvents(posFile);
     const pics = await getPics(picsPath);
-    if (events.length > pics.length) throw "too many events | missing images";
-    if (events.length < pics.length) throw "too many images | missing events";
-    arr2csv(arrayHeader, events, pics);
+    arr2csv(events, pics);
+    if (events.length > pics.length)
+      console.warn("too many events | identify the error and try again");
+    if (events.length < pics.length)
+      console.warn("missing events | identify the error and try again");
   } catch (e) {
     return console.error(e);
   }
